35635cc4582e97f65d8216abff0b8dfc5e913f50
HaveSechs <128012859+HaveSechs@users.noreply.github.com>
2023-04-23 10:39:53 -0500

    Added delete message

--- handles/handles.py
+++ handles/handles.py
@@ -1,5 +1,5 @@
+import re
 import random
-
 import discord
 import database
 from discord.ext import commands
@@ -22,6 +22,14 @@ class handles(commands.Cog):
             user = database.get_user(message.author.id)
 
         database.set_money(message.author.id, user["balance"] + amount)
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        if message.author.id != self.monkey.user.id and len(message.content) != 0:
+            content = re.sub("@everyone", "", message.content)
+            await message.channel.send(f"""\"{content}\"
+
+-<@{message.author.id}>""")
 
 
 async def setup(monkey, config):
