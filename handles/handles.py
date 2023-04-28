1e1e9f6aecca4d5dee3c56413e70a03eb0ffff47
HaveSechs <128012859+HaveSechs@users.noreply.github.com>
2023-04-28 18:38:26 -0500

    fixed exploit

--- handles/handles.py
+++ handles/handles.py
@@ -10,13 +10,26 @@ class handles(commands.Cog):
     def __init__(self, monkey, config):
         self.monkey = monkey
         self.config = config
+        self.invites = None
 
+    """
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # you can make this bot for multiple servers if you want but I made it only for mine
+        target = self.monkey.get_guild(self.config["guild"])
+        self.invites = await target.invites()
+    """
     @commands.Cog.listener()
     async def on_member_join(self, member):
         try:
             await member.send("**5 invites = Free Admin**\n\nAnyways meet some nigers while ur here")
         except:
             print(f"Could not dm {member.name}")
+
+        target = self.monkey.get_guild(self.config["guild"])
+        invites = await target.invites()
+
+        # for invite in invites:
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -30,11 +43,20 @@ class handles(commands.Cog):
 
         database.set_money(message.author.id, user["balance"] + amount)
 
+        if message.author.id == 302050872383242240:
+            user = database.get_user(message.interaction.user.id)
+            if user is None:
+                database.new_user(message.author.id, 500)
+            else:
+                database.set_money(message.interaction.user.id, user["balance"] + 500)
+            await message.channel.send("Added 500!")
+
     @commands.Cog.listener()
     async def on_message_delete(self, message):
         if message.author.id != self.monkey.user.id and len(message.content) != 0:
             content = re.sub("@everyone", "", message.content)
             await message.channel.send(f"""# \"{content}\"
+    
 
 # -<@{message.author.id}>""")
 
