e620171298d68f2d3a6b429cf9aabcad87575dfd
HaveSechs <sachseksdee@gmail.com>
2023-05-12 23:15:34 -0500

    made it look better (i'll patch the exploit next update)

--- visuals/eco.py
+++ visuals/eco.py
@@ -19,11 +19,24 @@ class dailyVisual(discord.ui.View):
 
         if coin_flip == 1:
             database.set_money(interaction.user.id, user["balance"] - 100)
-            await interaction.response.edit_message(content="for not knowing math i take away 100")
+            await interaction.response.edit_message(content="for not knowing math i take away 100", view=dailyVisualDisable())
         else:
             database.set_money(interaction.user.id, user["balance"] + 100)
-            await interaction.response.edit_message(content="ok enjoy ur 100 coin!!!")
+            await interaction.response.edit_message(content="ok enjoy ur 100 coin!!!", view=dailyVisualDisable())
 
     @discord.ui.button(label="no", style=discord.ButtonStyle.red)
     async def no(self, interaction: discord.Interaction):
         await interaction.response.edit_message(content="so smart!!!")
+
+
+class dailyVisualDisable(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.button(label="yes", style=discord.ButtonStyle.green, disabled=True)
+    async def yes(self):
+        pass
+
+    @discord.ui.button(label="no", style=discord.ButtonStyle.red, disabled=True)
+    async def no(self):
+        pass
